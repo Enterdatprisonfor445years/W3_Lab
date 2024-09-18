14080dd3cf98e8d4398cea751aeee3938ef7cbcb
09014230 <85706754+Enterdatprisonfor445years@users.noreply.github.com>
2024-09-18 12:14:06 -0400

    Vid 2.10-11

--- Lab_W3/Assets/Mover.cs
+++ Lab_W3/Assets/Mover.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class Newva : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
-   void Start()
+    void Start()
     {
-       
+        PrintInstructions();
     }
 
     void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls!");
+    }
+
+    void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
