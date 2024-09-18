6dc742c4398cc4d2adaee17d802177c1c3803f9b
09014230 <85706754+Enterdatprisonfor445years@users.noreply.github.com>
2024-09-18 13:57:56 -0400

    Mods

--- Lab_W3/Assets/Scripts/Mover.cs
+++ Lab_W3/Assets/Scripts/Mover.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 public class Newva : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float jumpStrength = 7f;
+    [SerializeField] LayerMask groundFloor;
+
+    Rigidbody rb;
+    bool isGrounded;
+
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         PrintInstructions();
     }
 
     void Update()
     {
         MovePlayer();
+        Jump();
     }
 
     void PrintInstructions()
     {
 
-        Debug.Log("Welcome to the game");
-        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Move your player with WASD or arrow keys!");
+        Debug.Log("Press Space to Jump! It will help jump over and avoid obstacles!");
         Debug.Log("Don't hit the walls!");
     }
 
@@ -29,5 +38,17 @@ public class Newva : MonoBehaviour
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         
         transform.Translate(xValue, 0, zValue);
+    }
+
+    void Jump()
+    {
+ 
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f, groundFloor);
+
+        // Jump when player presses spacebar and is grounded
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
+        }
     }
 }
